2ab2643b18c0647e1d90579d7085434d88ad945b
Suresha Ramappa <suriram.sam@gmail.com>
2022-02-07 17:12:06 +0530

    Routes to Create and get Ideas

--- server/config/router.js
+++ server/config/router.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const { ideas } = require("../controller");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send("Hey 👋🏻");
 });
+
+router.post("/ideas", ideas.createIdea);
+router.get("/ideas", ideas.getIdeas);
 
 module.exports = router;
