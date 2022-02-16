f2d33a8a26ab8f81b84e5c08aef1413060ab7927
Suresha Ramappa <suriram.sam@gmail.com>
2022-02-16 10:45:42 +0530

    Use mongo cluster instead local database

--- server/config/database.js
+++ server/config/database.js
@@ -1,15 +1,14 @@
 const mongoose = require("mongoose");
 
 const connect = async () => {
-  const { DB_SERVER, DB_PORT, DATABASE } = process.env;
+  const { URI } = process.env;
   try {
-    const uri = `mongodb://${DB_SERVER}:${DB_PORT}/${DATABASE}`;
     const options = {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     };
 
-    await mongoose.connect(uri, options);
+    await mongoose.connect(URI, options);
   } catch (error) {
     throw error;
   }
