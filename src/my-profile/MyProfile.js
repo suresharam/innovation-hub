050345aadb4cf8647c119755105b8454eba3c96f
Nihar Anil H <90025243+niharanilh@users.noreply.github.com>
2022-02-25 11:46:57 +0530

    myprofile changes

--- src/my-profile/MyProfile.js
+++ src/my-profile/MyProfile.js
@@ -1,8 +1,34 @@
 function MyProfile() {
     return (
-      <div>
+      <div class="first">
         <h2>Dashboard</h2>
+        <div class="second">
+        <label for="name">Name </label>
+         <input type="text" name="name" id="name" placeholder="Enter name" required/>
+        </div>
+        <br>
+        </br>
+
+
+        <div>
+        <label for="email">Email </label>
+         <input type="text" name="email" id="email" placeholder="Enter email" required/>
+
+        </div>
+        <br>
+        </br>
+
+        <div>
+        <label for="role">Role  </label>
+         <input type="text" name="role" id="role" placeholder="Enter role" required/>
+
+        </div>
+        <br>
+        </br>
+        <input type="submit" class="button-33" value="Submit"/>
+
       </div>
+      
     );
   }
 
