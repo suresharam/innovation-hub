b454f97cf0b6114d125f8ffd978a9b5fd33c6678
Suresha Ramappa <suriram.sam@gmail.com>
2022-02-25 13:55:01 +0530

    rename folders

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import * as React from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
-import Home from './home/Home';
-import Hackathon from './hackathon/Hackathon';
-import MyProfile from './my-profile/MyProfile';
-import MostLikedIdeas from './gem-recordings/GemRecordings';
-import WYWIdeas from './wyw-ideas/WYWIdeas';
-import MenuAppBar from './header/MenuAppBar';
-import Footer from './footer/Footer';
+import * as React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./home/Home";
+import Hackathon from "./hackathon/Hackathon";
+import MyProfile from "./my-profile/MyProfile";
+import MostLikedIdeas from "./gem-recordings/GemRecordings";
+import WYWIdeas from "./wyw-ideas/WYWIdeas";
+import MenuAppBar from "./header/MenuAppBar";
+import Footer from "./footer/Footer";
 
-
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
@@ -23,11 +18,11 @@ export default function App() {
           <MenuAppBar />
         </header>
         <Routes>
-          <Route exact path="/" element={<Home/>} />
-          <Route path="/hackathon" element={<Hackathon/>} />
-          <Route path="/myProfile" element={<MyProfile/>} />
-          <Route path="/gemRecordings" element={<MostLikedIdeas/>} />
-          <Route path="/wywIdeas" element={<WYWIdeas/>} />
+          <Route exact path="/" element={<Home />} />
+          <Route path="/hackathon" element={<Hackathon />} />
+          <Route path="/myProfile" element={<MyProfile />} />
+          <Route path="/gemRecordings" element={<MostLikedIdeas />} />
+          <Route path="/wywIdeas" element={<WYWIdeas />} />
         </Routes>
         <footer>
           <Footer />
